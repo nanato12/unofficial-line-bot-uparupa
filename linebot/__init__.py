55435b6f092d4d8998dd04f682a19644b9a6c468
nanato12 <admin@okj.info>
2024-03-16 15:20:08 +0900

    update: LINEBot

--- linebot/__init__.py
+++ linebot/__init__.py
@@ -1,3 +1,6 @@
+from threading import Thread
+from time import sleep
+
 from CHRLINE import CHRLINE
 from CHRLINE.hooks import HooksTracer
 
@@ -7,10 +10,20 @@ from linebot.parser import ConfigParser
 
 class LINEBot(metaclass=SingletonMeta):
     auth_token: str
+    bot: CHRLINE
     tracer: HooksTracer
 
     def __init__(self) -> None:
         parser: ConfigParser = ConfigParser()
-        c = CHRLINE(parser.token, device=parser.device, useThrift=True)
-        self.auth_token = c.authToken
-        self.tracer = HooksTracer(c, prefixes=["/"])
+
+        self.bot = CHRLINE(parser.token, device=parser.device, useThrift=True)
+        self.auth_token = self.bot.authToken
+        self.tracer = HooksTracer(self.bot, prefixes=["/"])
+
+        t = Thread(target=self.noop)
+        t.daemon = True
+        t.start()
+
+    def noop(self) -> None:
+        self.bot.noop()
+        sleep(20)
