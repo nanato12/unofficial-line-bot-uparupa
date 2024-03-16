9157ec402f1b444f33097569299399f4e5ec3b82
nanato12 <admin@okj.info>
2024-03-16 12:45:44 +0900

    update: TestCommandHook

--- linebot/hook/command/test.py
+++ linebot/hook/command/test.py
@@ -3,6 +3,7 @@ from CHRLINE.hooks import HooksTracer
 from CHRLINE.services.thrift.ttypes import Message
 
 from linebot import LINEBot
+from linebot.flex.profile import ProfileFlex
 from linebot.logger import get_file_path_logger
 
 logger = get_file_path_logger(__name__)
@@ -12,8 +13,12 @@ tracer = line.tracer
 
 
 class TestCommandHook(HooksTracer):
-    @tracer.Command()
+    @tracer.Command(inpart=True)
     def test(self, msg: Message, bot: CHRLINE) -> None:
         """Test."""
 
         bot.replyMessage(msg, str(msg))
+        bot.sendLiff(
+            msg.to,
+            ProfileFlex().build_message(),
+        )
