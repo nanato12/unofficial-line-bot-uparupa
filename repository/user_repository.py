8c1408c8993b5202d2a4cb178d9df1c50ea79146
nanato12 <admin@okj.info>
2024-03-16 18:17:28 +0900

    update: user_repository

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,8 +1,7 @@
 from typing import Optional
 
-from database.engine import session
 from database.models.user import User
 
 
 def find_user_from_mid(mid: str) -> Optional[User]:
-    return session.query(User).filter(User.mid == mid).first()
+    return User.query.filter(User.mid == mid).first()
