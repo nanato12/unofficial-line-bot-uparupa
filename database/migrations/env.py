9a5aac13d5f6c91cdea04dacffa97f9ef2ae4300
nanato12 <admin@okj.info>
2024-03-16 14:58:04 +0900

    add: migration

--- database/migrations/env.py
+++ database/migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from database.engine import Base
 from linebot.parser import ConfigParser
+from database import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
