4cf699cef3edb505b47ea00c20ab82e0fb51f61e
nanato12 <admin@okj.info>
2024-03-16 22:34:27 +0900

    update: TestCommandHook

--- linebot/hook/command/test.py
+++ linebot/hook/command/test.py
@@ -1,10 +1,11 @@
 from CHRLINE import CHRLINE
 from CHRLINE.hooks import HooksTracer
-from CHRLINE.services.thrift.ttypes import Message
+from CHRLINE.services.thrift.ttypes import Contact, Message
 
 from linebot import LINEBot
 from linebot.flex.profile import ProfileFlex
 from linebot.logger import get_file_path_logger
+from repository.user_repository import get_or_create_user_from_contact
 
 logger = get_file_path_logger(__name__)
 
@@ -17,8 +18,10 @@ class TestCommandHook(HooksTracer):
     def test(self, msg: Message, bot: CHRLINE) -> None:
         """Test."""
 
-        bot.replyMessage(msg, str(msg))
+        c: Contact = bot.getContact(str(msg._from))
+        u = get_or_create_user_from_contact(c)
+
         bot.sendLiff(
             msg.to,
-            ProfileFlex().build_message(),
+            ProfileFlex(u).build_message(),
         )
