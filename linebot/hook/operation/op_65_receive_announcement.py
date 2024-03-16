ceaf54a54e18251ce5dd514146a1f372e900e68a
nanato12 <admin@okj.info>
2024-03-16 15:49:27 +0900

    add: Op65Hook

---
+++ linebot/hook/operation/op_65_receive_announcement.py
@@ -0,0 +1,21 @@
+from CHRLINE import CHRLINE
+from CHRLINE.hooks import HooksTracer
+from CHRLINE.services.thrift.ttypes import Operation, OpType
+
+from database.models.operation import Operation as OperationModel
+from linebot import LINEBot
+from linebot.logger import get_file_path_logger
+
+logger = get_file_path_logger(__name__)
+
+line = LINEBot()
+tracer = line.tracer
+
+
+class Op65Hook(HooksTracer):
+    @tracer.Operation(OpType.NOTIFIED_DESTROY_MESSAGE)
+    def receive_announcement(self, op: Operation, bot: CHRLINE) -> None:
+        logger.info(op)
+
+        o = OperationModel.from_line_operation(op)
+        o.save()
