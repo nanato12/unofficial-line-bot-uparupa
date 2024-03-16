66b122adcb1b5c43039d1f659fa05ce5f49d2f9b
nanato12 <admin@okj.info>
2024-03-16 23:11:34 +0900

    update: ProfileFlex

--- linebot/flex/profile/__init__.py
+++ linebot/flex/profile/__init__.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from database.models.user import User
 from linebot.flex import BaseFlex
+from linebot.helpers.calculation import calc_need_exp
 
 
 @dataclass
@@ -15,14 +16,33 @@ class ProfileFlex(BaseFlex):
         content = self.get_flex_content()
         u: User = self.user
 
-        # icon
-        content["contents"][0]["body"]["contents"][0]["contents"][0][
+        profile_content = content["contents"][0]["body"]["contents"][0][
             "contents"
-        ][0]["url"] = u.profile_url
+        ]
+
+        # icon
+        profile_content[0]["contents"][0]["url"] = u.profile_url
 
         # name
-        content["contents"][0]["body"]["contents"][0]["contents"][1][
+        profile_content[1]["contents"][0]["contents"][0]["contents"][0][
+            "text"
+        ] = u.name
+
+        # level
+        profile_content[1]["contents"][1]["contents"][0]["contents"][0][
             "contents"
-        ][0]["contents"][0]["contents"][0]["text"] = u.name
+        ][0]["contents"][1]["text"] = str(u.level)
+
+        # exp
+        profile_content[1]["contents"][1]["contents"][0]["contents"][1][
+            "contents"
+        ][0]["contents"][1]["text"] = f"{u.exp:,}"
+
+        # need exp
+        profile_content[1]["contents"][1]["contents"][0]["contents"][1][
+            "contents"
+        ][0]["contents"][3][
+            "text"
+        ] = f"{calc_need_exp(u.exp):,}"  # type: ignore
 
         return content
