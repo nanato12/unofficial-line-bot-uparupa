cfcccc6108cbce8f59aee806dda2d1be9fa0f4f5
nanato12 <admin@okj.info>
2024-03-16 14:58:19 +0900

    update: OpHooks

--- linebot/hook/operation/op_26_receive_message.py
+++ linebot/hook/operation/op_26_receive_message.py
@@ -2,6 +2,7 @@ from CHRLINE import CHRLINE
 from CHRLINE.hooks import HooksTracer
 from CHRLINE.services.thrift.ttypes import Operation, OpType
 
+from database.models.operation import Operation as OperationModel
 from linebot import LINEBot
 from linebot.logger import get_file_path_logger
 
@@ -15,3 +16,5 @@ class Op26Hook(HooksTracer):
     @tracer.Operation(OpType.RECEIVE_MESSAGE)
     def receive_message(self, op: Operation, bot: CHRLINE) -> None:
         tracer.trace(op.message, self.HooksType["Content"], bot)
+        o = OperationModel.from_line_operation(op)
+        o.save()
