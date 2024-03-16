1ce297e4714e78db1541f47f4a5986d1ac41cdde
nanato12 <admin@okj.info>
2024-03-16 17:26:45 +0900

    update: Op124Hook

--- linebot/hook/operation/op_124_notified_invite_into_chat.py
+++ linebot/hook/operation/op_124_notified_invite_into_chat.py
@@ -1,6 +1,8 @@
+from time import sleep
+
 from CHRLINE import CHRLINE
 from CHRLINE.hooks import HooksTracer
-from CHRLINE.services.thrift.ttypes import Operation, OpType
+from CHRLINE.services.thrift.ttypes import Contact, Operation, OpType
 
 from database.models.operation import Operation as OperationModel
 from linebot import LINEBot
@@ -16,6 +18,15 @@ class Op124Hook(HooksTracer):
     @tracer.Operation(OpType.NOTIFIED_INVITE_INTO_CHAT)
     def notified_invite_into_chat(self, op: Operation, bot: CHRLINE) -> None:
         logger.info(op)
+
+        gid = str(op.param1)
+        inviter_mid = str(op.param2)
+
+        if bot.mid in str(op.param3).split():
+            bot.acceptChatInvitation(gid)
+            contact: Contact = bot.getContact(inviter_mid)
+            sleep(5)
+            bot.sendMention(gid, "@! 招待ありがとう♪", mids=[contact.mid])
 
         o = OperationModel.from_line_operation(op)
         o.save()
