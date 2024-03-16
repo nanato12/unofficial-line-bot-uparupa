36f7555a5906c683afef68e1ba4790ffd9a0e8a2
nanato12 <admin@okj.info>
2024-03-16 22:34:14 +0900

    add: get_or_create_user_from_contact

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,7 +1,17 @@
 from typing import Optional
 
+from CHRLINE.services.thrift.ttypes import Contact
+
 from database.models.user import User
 
 
 def find_user_from_mid(mid: str) -> Optional[User]:
     return User.query.filter(User.mid == mid).first()
+
+
+def get_or_create_user_from_contact(c: Contact) -> User:
+    u = find_user_from_mid(str(c.mid))
+    if not u:
+        u = User.from_line_contact(c)
+        u.create()
+    return u
