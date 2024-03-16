e43be9592a6373f8abab90483a636163dd662d62
nanato12 <admin@okj.info>
2024-03-16 22:36:41 +0900

    update: ProfileFlex

--- linebot/flex/profile/__init__.py
+++ linebot/flex/profile/__init__.py
@@ -1,10 +1,28 @@
+from dataclasses import dataclass
 from typing import Any
 
+from database.models.user import User
 from linebot.flex import BaseFlex
 
 
+@dataclass
 class ProfileFlex(BaseFlex):
+    user: User
+
     __alt_text__ = "Profile Flex Message"
 
     def build(self) -> dict[str, Any]:
-        return self.get_flex_content()
+        content = self.get_flex_content()
+        u: User = self.user
+
+        # icon
+        content["contents"][0]["body"]["contents"][0]["contents"][0][
+            "contents"
+        ][0]["url"] = u.profile_url
+
+        # name
+        content["contents"][0]["body"]["contents"][0]["contents"][1][
+            "contents"
+        ][0]["contents"][0]["contents"][0]["text"] = u.name
+
+        return content
