6ecd8ad46079d8466082db2e9f2c1843789c9862
Haruto <admin@okj.info>
2024-03-16 11:20:28 +0900

    SQLAlchemyを使用する (#2)

---
+++ database/engine.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from datetime import datetime
+
+from inflection import pluralize
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declared_attr, sessionmaker
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+from sqlalchemy.schema import Column
+from sqlalchemy.types import DateTime, Integer
+
+from linebot.parser import ConfigParser
+
+c = ConfigParser.get_config_by_name("default")
+engine = create_engine(
+    "mysql://{user}:{password}@{host}:{port}/{database}?charset={charset}".format(
+        user=c["db_user"],
+        password=c["db_password"],
+        host=c["db_host"],
+        port=c["db_port"],
+        database=c["db_database"],
+        charset=c["db_charset"],
+    )
+)
+
+SessionClass = sessionmaker(engine)
+session = SessionClass()
+
+
+class BaseModel:
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    updated_at = Column(
+        DateTime,
+        default=datetime.now(),
+        onupdate=datetime.now(),
+        nullable=False,
+    )
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return pluralize(cls.__name__.lower())  # type: ignore
+
+    def save(self) -> None:
+        session.add(self)
+        session.commit()
+
+
+Base: DeclarativeMeta = declarative_base(cls=BaseModel)
--- linebot/parser.py
+++ linebot/parser.py
@@ -9,19 +9,51 @@ DEFAULT_CONFIG_JSON = "config.json"
 
 
 class ConfigParser(ArgumentParser, metaclass=SingletonMeta):
-    config_name: str
     device: str = ""
     token: str = ""
     log_name: str = ""
+    db_host: str = ""
+    db_port: str = ""
+    db_user: str = ""
+    db_password: str = ""
+    db_database: str = ""
+    db_charset: str = ""
 
     def __init__(self) -> None:
         super().__init__()
-        self.add_argument(
-            "-c", "--config-name", default="default", help="設定名"
-        )
+        self.add_argument("-c", "--config-name", default="", help="設定名")
+
+        # linebot arguments
         self.add_argument("-d", "--device", help="デバイス名")
         self.add_argument("-t", "--token", help="トークン")
         self.add_argument("-l", "--log-name", help="ログファイル名")
+
+        # database arguments
+        self.add_argument(
+            "-dh", "--db-host", default="127.0.0.1", help="DBのホスト"
+        )
+        self.add_argument(
+            "-dp", "--db-port", default="3306", help="DBのポート"
+        )
+        self.add_argument(
+            "-du", "--db-user", default="admin", help="DBのユーザー名"
+        )
+        self.add_argument(
+            "-dpass",
+            "--db-password",
+            default="password",
+            help="DBのパスワード",
+        )
+        self.add_argument(
+            "-dd",
+            "--db-database",
+            default="nanato12_linebot",
+            help="DBのデータベース名",
+        )
+        self.add_argument(
+            "-dc", "--db-charset", default="utf8mb4", help="DBの文字コード"
+        )
+
         self.__parse()
 
     def __parse(self) -> None:
@@ -35,16 +67,22 @@ class ConfigParser(ArgumentParser, metaclass=SingletonMeta):
             if v is not None:
                 setattr(self, arg, v)
 
+        for var in self.class_vars.keys():
+            if var in ["token"]:
+                continue
+            if getattr(self, var) == "":
+                raise Exception(f"'{var}' argument variable not set.")
+
     def __parse_by_config(self, config_name: str) -> None:
         config = self.get_config_by_name(config_name)
 
-        self_vars: dict[str, type] = vars(self.__class__).get(
-            "__annotations__", {}
-        )
-
-        for var, t in self_vars.items():
+        for var, t in self.class_vars.items():
             if isinstance(v := config.get(var), t) and v:
                 setattr(self, var, v)
+
+    @property
+    def class_vars(self) -> dict[str, type]:
+        return vars(self.__class__).get("__annotations__", {})
 
     @staticmethod
     def get_config_by_name(config_name: str) -> dict[str, Any]:
