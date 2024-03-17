38c323b4d58f45dc40097b6bb24acd7dfb840939
nanato12 <admin@okj.info>
2024-03-17 11:50:39 +0900

    add: get_user_from_mid

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -3,10 +3,18 @@ from typing import Optional
 from CHRLINE.services.thrift.ttypes import Contact
 
 from database.models.user import User
+from repository.exceptions import NotFoundRecordError
 
 
 def find_user_from_mid(mid: str) -> Optional[User]:
     return User.query.filter(User.mid == mid).first()
+
+
+def get_user_from_mid(mid: str) -> User:
+    u = find_user_from_mid(str(mid))
+    if not u:
+        raise NotFoundRecordError(f"mid: '{mid}' is not found.")
+    return u
 
 
 def get_or_create_user_from_contact(c: Contact) -> User:
