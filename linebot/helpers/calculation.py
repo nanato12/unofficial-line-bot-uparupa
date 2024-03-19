d9d2667d32ba3bdb70a0fe4f8154b6ab815cd522
nanato12 <admin@okj.info>
2024-03-19 10:00:50 +0900

    add: random_exp

--- linebot/helpers/calculation.py
+++ linebot/helpers/calculation.py
@@ -1,5 +1,10 @@
 from math import ceil
+from random import randint
 
 
 def calc_need_exp(level: int) -> int:
     return ceil(100 * level + 0.25 * level**2)
+
+
+def random_exp() -> int:
+    return randint(15, 35)
