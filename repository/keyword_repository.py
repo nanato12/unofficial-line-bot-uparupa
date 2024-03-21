9c295c8286c4ad6198f7b22cea68e04f7d87fb94
nanato12 <admin@okj.info>
2024-03-21 15:40:26 +0900

    add: keyword_repository

---
+++ repository/keyword_repository.py
@@ -0,0 +1,49 @@
+from random import choices
+from typing import Optional
+
+from constants.enums.authority import Authority
+from database.models.keyword import Keyword
+from database.models.user import User
+
+
+def find_keywords_from_receive_text(s: str) -> list[Keyword]:
+    return Keyword.query.filter(Keyword.receive_text == s).all()
+
+
+def extract_admin_keyword(keywords: list[Keyword]) -> Optional[Keyword]:
+    admin_keywords = [
+        k for k in keywords if k.registrant.authority == Authority.ADMIN
+    ]
+    if admin_keywords:
+        return admin_keywords[0]
+
+
+def choice_keyword(keywords: list[Keyword]) -> Optional[Keyword]:
+    """複数のキーワード候補から一つ絞り込む関数
+    ---
+    権限によって返信確率が定義されているため、確率が当たらなかった場合はNoneが返る
+
+    Args:
+        keywords (list[Keyword]): キーワード候補リスト
+
+    Returns:
+        Optional[Keyword]: キーワード
+    """
+    admin_keyword = extract_admin_keyword(keywords)
+    if admin_keyword:
+        return admin_keyword
+
+    # 返信比率の重みづけを算出する（権限レベルxユーザーレベル）
+    users: list[User] = [k.registrant for k in keywords]
+    weights = [u.authority * u.level for u in users]
+
+    # Noneの重み
+    keywords.append(None)
+    weights.append(int(sum(weights) / 2))
+
+    return choices(keywords, weights=weights)[0]
+
+
+def check_registration_keyword(u: User, text: str) -> bool:
+    keywords: list[Keyword] = u.keywords
+    return bool(list(filter(lambda x: x.receive_text == text, keywords)))
