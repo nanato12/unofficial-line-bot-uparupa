6013ce06dcbb5e588393b7ef3f802f105c98c9b5
nanato12 <admin@okj.info>
2024-03-22 22:37:24 +0900

    add: get_ranked_users

--- database/models/user.py
+++ database/models/user.py
@@ -6,7 +6,7 @@ from urllib.parse import urljoin
 
 from CHRLINE.config import Config
 from CHRLINE.services.thrift.ttypes import Contact as ThriftContact
-from sqlalchemy import Enum, Text, desc, text
+from sqlalchemy import Enum, Text, text
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, String
 
@@ -15,6 +15,7 @@ from database.engine import Base, Session
 from linebot.config import Config as LINEBotConfig
 from linebot.helpers.calculation import calc_need_exp, random_exp
 from repository.message_repository import find_user_group_last_message
+from repository.user_repository import get_ranked_users
 
 
 class User(Base):
@@ -78,14 +79,7 @@ class User(Base):
         if self.authority == Authority.ADMIN:
             return 0
 
-        return [
-            u.mid
-            for u in User.query.filter(User.authority != Authority.ADMIN)
-            .order_by(desc(User.level))
-            .order_by(desc(User.exp))
-            .order_by(desc(User.created_at))
-            .all()
-        ].index(self.mid) + 1
+        return [u.mid for u in get_ranked_users()].index(self.mid) + 1
 
     def can_give_exp(self, text: str, gid: str) -> bool:
         last_message = find_user_group_last_message(self.mid, gid)
