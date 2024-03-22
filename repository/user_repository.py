8f8f20eb44135558611c9bea2a889dbe7d8f3f84
nanato12 <admin@okj.info>
2024-03-23 02:19:00 +0900

    add: find_user_from_name

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -23,3 +23,7 @@ def get_or_create_user_from_mid(mid: str, bot: CHRLINE) -> User:
         u = User.from_line_contact(bot.getContact(mid))
         u.create()
     return u
+
+
+def find_user_from_name(n: str) -> Optional[User]:
+    return User.query.filter(User.name == n).first()  # type: ignore[no-any-return]
