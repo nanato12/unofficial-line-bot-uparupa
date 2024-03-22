d19b02d21c8e04d92eaee5e6bc4621dce56ce4aa
nanato12 <admin@okj.info>
2024-03-22 12:59:07 +0900

    update: HooksTracerWrapper

--- linebot/wrappers/user_hook_tracer.py
+++ linebot/wrappers/user_hook_tracer.py
@@ -2,9 +2,6 @@ from datetime import datetime
 
 from CHRLINE.hooks import HooksTracer
 
-from database.models.user import User
-
 
 class HooksTracerWrapper(HooksTracer):
-    user: User
     setup_timestamp: datetime = datetime.now()
