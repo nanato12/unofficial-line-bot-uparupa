253e44434b93a6ad282219a8febacfb45bf52c3f
nanato12 <admin@okj.info>
2024-03-28 13:52:54 +0900

    update: EmojiCommandHook

--- linebot/hook/command/emoji.py
+++ linebot/hook/command/emoji.py
@@ -1,8 +1,10 @@
+from random import sample as random_sample
+
 from CHRLINE.services.thrift.ttypes import Message
 
 from linebot.line import LINEBot
 from linebot.logger import get_file_path_logger
-from linebot.models.emojis.mahjang import MahjangMUPEmoji
+from linebot.models.emojis.mahjang import PAI, MahjangMUPEmoji
 from linebot.models.emojis.slot_emoji import SlotEmoji
 from linebot.wrappers.chrline_wrapper import CHRLINEWrapper
 from linebot.wrappers.hook_tracer_wrapper import HooksTracerWrapper
@@ -47,3 +49,12 @@ class EmojiCommandHook(HooksTracerWrapper):
         bot.reply_emojis_message(
             msg, *MahjangMUPEmoji.convert_message(target_text.split(" "))
         )
+
+    @tracer.Command(prefixes=False, alt=["ななといつ"])
+    def nanato12(self, msg: Message, bot: CHRLINEWrapper) -> None:
+        """
+        七対子を送信します
+        """
+        pai = random_sample(PAI, k=7)
+        pai = sorted(pai + pai)
+        bot.reply_emojis_message(msg, *MahjangMUPEmoji.convert_message(pai))
