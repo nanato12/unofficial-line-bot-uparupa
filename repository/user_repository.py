44f99027fd0e7a231159db6aca4a1d177dec0c11
nanato12 <admin@okj.info>
2024-03-28 10:41:05 +0900

    add: get_or_create_user_from_contact

--- repository/user_repository.py
+++ repository/user_repository.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from CHRLINE import CHRLINE
+from CHRLINE.services.thrift.ttypes import Contact
 
 from database.models.user import User
 from linebot.exceptions.sql_error import NotFoundRecordError
@@ -14,6 +15,14 @@ def get_user_from_mid(mid: str) -> User:
     u = find_user_from_mid(str(mid))
     if not u:
         raise NotFoundRecordError(f"mid: '{mid}' is not found.")
+    return u
+
+
+def get_or_create_user_from_contact(c: Contact) -> User:
+    u = find_user_from_mid(c.mid)
+    if not u:
+        u = User.from_line_contact(c)
+        u.create()
     return u
 
 
