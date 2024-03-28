80070cd25abad857e2aa1ce6e6334de36a6d762b
nanato12 <admin@okj.info>
2024-03-28 12:30:04 +0900

    Add: CHRLINEWrapper

--- linebot/wrappers/chrline_wrapper.py
+++ linebot/wrappers/chrline_wrapper.py
@@ -1,9 +1,17 @@
 from json import dumps as json_dumps
+from json import loads as json_loads
+from typing import Union
 
 from CHRLINE import CHRLINE
-from CHRLINE.services.thrift.ttypes import Message
+from CHRLINE.services.thrift.ttypes import Message, MIDType
 
+from database.models.user import User
+from linebot.flex.profile import ProfileFlex
+from linebot.helpers.calculation import calc_need_exp
+from linebot.logger import get_file_path_logger
 from linebot.models.emojis import Emoji
+
+logger = get_file_path_logger(__name__)
 
 
 class CHRLINEWrapper(CHRLINE):
@@ -33,3 +41,25 @@ class CHRLINEWrapper(CHRLINE):
                 ),
             },
         )
+
+    def reply_user_profile(self, msg: Message, u: User) -> Union[str, Message]:
+        to: str = msg._from if msg.toType == MIDType.USER else msg.to
+
+        logger.info(
+            r := self.sendLiff(
+                to,
+                ProfileFlex(u).build_message(),
+                liffId="1626444543-G6O9lb5v",
+            )
+        )
+
+        if json_loads(r).get("status") != "ok":
+            self.replyMessage(
+                msg,
+                (
+                    f"Ranking: {u.ranking}\n"
+                    f"権限: {u.authority.name}\n"
+                    f"レベル: {u.level}\n"
+                    f"経験値: {u.exp:,} / {calc_need_exp(u.level):,}"
+                ),
+            )
