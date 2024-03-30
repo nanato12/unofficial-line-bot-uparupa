03a24979b0d37259b71380166a1092a48a01d6f2
Haruto <admin@okj.info>
2024-03-30 18:04:27 +0900

    既読確認機能の修正 (#41)

--- repository/operation_repository.py
+++ repository/operation_repository.py
@@ -1,11 +1,23 @@
 from CHRLINE.services.thrift.ttypes import OpType
+from sqlalchemy import func
 
+from database.models.message import Message
 from database.models.operation import Operation
 
 
 def get_read_message_ops(message_id: str) -> list[Operation]:
+    message = Message.query.filter(Message.message_id == message_id).first()
+    if not message:
+        return []
+
     return (  # type: ignore[no-any-return]
         Operation.query.filter(Operation.type == OpType.NOTIFIED_READ_MESSAGE)
-        .filter(Operation.param3 == message_id)
+        .filter(Operation.param1 == message.to)
+        .filter(Operation.created_time >= message.create_time)
+        .group_by(Operation.param2)
+        .with_entities(
+            func.min(Operation.created_at).label("created_at"),
+            Operation.param2,
+        )
         .all()
     )
