ea9feea850e5b523f8a71ee55882ab512dcc5e54
nanato12 <admin@okj.info>
2024-03-30 10:33:09 +0900

    add: GPT command

--- linebot/hook/command/common.py
+++ linebot/hook/command/common.py
@@ -3,6 +3,11 @@ from typing import Optional
 from CHRLINE import CHRLINE
 from CHRLINE.services.thrift.ttypes import Message, MIDType
 
+from database.models.message import Message as MessageModel
+from gpt import GPT
+from gpt.message import Message as GPTMessage
+from gpt.model import Model
+from gpt.role import Role
 from linebot.line import LINEBot
 from linebot.logger import get_file_path_logger
 from linebot.wrappers.hook_tracer_wrapper import HooksTracerWrapper
@@ -50,3 +55,38 @@ class CommonCommandHook(HooksTracerWrapper):
             bot.unsendMessage(unsend_msg_id)
         else:
             bot.replyMessage(msg, "取り消したいメッセージをリプライしてね！")
+
+    @tracer.Command(inpart=True)
+    def gpt(self, msg: Message, bot: CHRLINE) -> None:
+        """
+        ChatGPTに質問を投げられます。
+        リプライでも使用可能です。
+        """
+
+        text: str = msg.text
+
+        if reply_message_id := msg.relatedMessageId:
+            reply_message = MessageModel.query.filter(
+                MessageModel.message_id == reply_message_id
+            ).first()
+            if not reply_message:
+                bot.replyMessage(
+                    msg, "リプライ先のメッセージが見つからなかったよ😭"
+                )
+                return
+
+            text = f"{reply_message.text}\n\n" + text.replace("/gpt", "")
+
+        gpt = GPT(
+            Model.GPT_35_TURBO,
+            [
+                GPTMessage(
+                    Role.SYSTEM,
+                    "あなたは うぱるぱ という名前です。"
+                    "あなたはYouChatではなく、うぱるぱです。",
+                )
+            ],
+        )
+
+        gpt.add_user_message(text)
+        bot.replyMessage(msg, gpt.create())
