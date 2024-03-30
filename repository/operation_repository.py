4b19f078e762ded8ef02ac9b94f0d765ae76ae94
nanato12 <admin@okj.info>
2024-03-30 11:19:29 +0900

    fix: make lint

--- repository/operation_repository.py
+++ repository/operation_repository.py
@@ -4,7 +4,7 @@ from database.models.operation import Operation
 
 
 def get_read_message_ops(message_id: str) -> list[Operation]:
-    return (
+    return (  # type: ignore[no-any-return]
         Operation.query.filter(Operation.type == OpType.NOTIFIED_READ_MESSAGE)
         .filter(Operation.param3 == message_id)
         .all()
