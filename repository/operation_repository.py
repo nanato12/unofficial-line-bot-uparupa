5763d2c1ead25e517a4bb48ecb468b3e9be68964
nanato12 <admin@okj.info>
2024-04-02 11:15:36 +0900

    update: get_read_message_ops

--- repository/operation_repository.py
+++ repository/operation_repository.py
@@ -1,5 +1,5 @@
 from CHRLINE.services.thrift.ttypes import OpType
-from sqlalchemy import func
+from sqlalchemy import asc, func
 
 from database.models.message import Message
 from database.models.operation import Operation
@@ -13,11 +13,13 @@ def get_read_message_ops(message_id: str) -> list[Operation]:
     return (  # type: ignore[no-any-return]
         Operation.query.filter(Operation.type == OpType.NOTIFIED_READ_MESSAGE)
         .filter(Operation.param1 == message.to)
+        .filter(Operation.param1 != message._from)
         .filter(Operation.created_time >= message.create_time)
         .group_by(Operation.param2)
         .with_entities(
             func.min(Operation.created_at).label("created_at"),
             Operation.param2,
         )
+        .order_by(asc("created_at"))
         .all()
     )
