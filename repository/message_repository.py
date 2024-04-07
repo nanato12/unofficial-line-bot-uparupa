bf3ca62bc7c4959663fd4b4232a99f98ccbdf457
nanato12 <admin@okj.info>
2024-04-08 04:15:07 +0900

    add: get_mention_message

--- repository/message_repository.py
+++ repository/message_repository.py
@@ -16,3 +16,15 @@ def find_user_group_last_message(mid: str, gid: str) -> Optional[Message]:
 
 def get_partial_match_messages(word: str) -> list[Message]:
     return Message.query.where(Message.text.like(f"%{word}%")).all()  # type: ignore[no-any-return]
+
+
+def get_mention_message(
+    mid: str, to: str, exclude_message_ids: list[int]
+) -> list[Message]:
+    return (
+        Message.query.filter(Message.to == to)
+        .filter(Message._from != mid)
+        .filter(Message.content_metadata.like("%MENTIONEES%"))
+        .filter(Message.id.not_in(exclude_message_ids))
+        .all()
+    )
